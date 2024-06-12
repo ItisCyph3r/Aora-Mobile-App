bf42249fad6c381cfd1502b4eb9fc253d75ef263
Momoh Osaze <76972139+ItisCyph3r@users.noreply.github.com>
2024-06-12 18:58:47 +0100

    onboarding screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,17 @@
 import { Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { Link } from "expo-router";
+
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }} className='flex'
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex items-center justify-center bg-grey-300 text-white mt-3">
+      <Text className="text-3xl font-pblack">
+        Zapnode
+      </Text>
+      <StatusBar style="auto" />
+      <Link href={'/profile'} className="text-blue-500"> Go to Profile</Link>
+      <Link href={'/home'} className="text-blue-500"> Go to Home</Link>
     </View>
   );
 }
