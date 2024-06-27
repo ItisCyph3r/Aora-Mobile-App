1cec2900edc7bb75fd52ddba47516b698a0a2a98
Momoh Osaze <76972139+ItisCyph3r@users.noreply.github.com>
2024-06-27 03:42:26 +0100

    trending video

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -18,34 +18,6 @@ const VideoCard = ({ video: { title, thumbnail, video, creator: { username, avat
     const [play, setPlay] = useState(false);
     return (
         <View className='flex flex-col items-center px-4 mb-14'>
-            <View className='flex flex-row gap-3 items-center'>
-                <View className='flex justify-center items-center flex-row flex-1'>
-                    <View className='w-[46px] h-[46px] rounded-lg border border-secondary justify-center items-center p-0.5'>
-                        <Image
-                            // source={{ uri: avatar }}
-                            source={{ uri: avatar }}
-                            resizeMode='cover'
-                            className='w-full h-full rounded-lg'
-                        />
-                    </View>
-                    <View className='flex justify-center flex-1 ml-3 gap-y-1'>
-                        <Text className='text-white font-psemibold text-sm' numberOfLines={1}>
-                            {title}
-                        </Text>
-                        <Text className='text-xs text-gray-100 font-pregular' numberOfLines={1}>
-                            {username}
-                        </Text>
-                    </View>
-                </View>
-
-                <View>
-                    <Image
-                        source={icons.menu}
-                        className='w-5 h-5'
-                        resizeMode='contain'
-                    />
-                </View>
-            </View>
             {play ? (
                     <Text> Playing </Text>
                 ): (
@@ -66,6 +38,35 @@ const VideoCard = ({ video: { title, thumbnail, video, creator: { username, avat
                         />
                     </TouchableOpacity>
                 )}
+            <View className='flex flex-row gap-3 items-center mt-3'>
+                <View className='flex justify-center items-center flex-row flex-1'>
+                    <View className='w-[46px] h-[46px] rounded-3xl border border-secondary justify-center items-center p-0.5'>
+                        <Image
+                            // source={{ uri: avatar }}
+                            source={{ uri: avatar }}
+                            resizeMode='cover'
+                            className='w-full h-full rounded-3xl'
+                        />
+                    </View>
+                    <View className='flex justify-center flex-1 ml-3 gap-y-1'>
+                        <Text className='text-white font-psemibold text-sm' numberOfLines={1}>
+                            {title}
+                        </Text>
+                        <Text className='text-xs text-gray-100 font-pregular' numberOfLines={1}>
+                            {username}
+                        </Text>
+                    </View>
+                </View>
+
+                <View>
+                    <Image
+                        source={icons.menu}
+                        className='w-5 h-5'
+                        resizeMode='contain'
+                    />
+                </View>
+            </View>
+            
         </View>
     )
 }
